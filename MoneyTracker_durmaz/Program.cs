8859f49c134a7b6e7b41f77f2e43a07b2ed5f18a
Muhammed Durmaz <durmaz.m02@htlwienwest.at>
2020-12-22 17:52:35 +0100

    Projektdateien hinzufügen.

---
+++ MoneyTracker_durmaz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoneyTracker_durmaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
